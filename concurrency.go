39b3a56fe24477ed782990a5f05acfb3e41fa579
Daniel Abati <danielabatibabatunde1@gmail.com>
2025-01-07 16:35:19 +0100

    learning about concurrncy, channels & buffers

---
+++ concurrency.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// the keyword "go" handles concurrent routine
+
+func sendEmail(message string) {
+	go func() {
+		time.Sleep(time.Millisecond * 250)
+		fmt.Printf("Email received:  '%s'\n", message)
+	}()
+	fmt.Printf("Email Sent: '%s'\n", message)
+}
+
+// using channels
+func filterOldEmails(emails []email) {
+	isOldChan := make(chan bool)
+
+	go func() {
+		for _,e :=range emails{
+			if e.date.Before(time.Date(2020, 0,0,0,0,0,0, time.U)) {
+					isOldChan <- true
+					continue
+			}
+			isOldchan <- false 
+		}
+	}()
+
+	isOld := <- isOldChan
+	fmt.Println("email 1 is Old: ", isOld)
+	isOld = <- isOldChan
+	fmt.Println("email 2 is Old: ", isOld)
+	isOld = <- isOldChan
+	fmt.Println("email 3 is Old: ", isOld)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Night-Prime/Learning-Go
+
+go 1.21.6
